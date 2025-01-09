01f5904799a55077de999614006a09ecec88fde2
geshang777 <yzy12468@126.com>
2025-01-09 14:25:05 +0000

    clear

--- focus/modeling/pixel_decoder/ops/functions/ms_deform_attn_func.py
+++ focus/modeling/pixel_decoder/ops/functions/ms_deform_attn_func.py
@@ -36,7 +36,6 @@ class MSDeformAttnFunction(Function):
         output = MSDA.ms_deform_attn_forward(
             value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, ctx.im2col_step)
         ctx.save_for_backward(value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights)
-
         return output
 
     @staticmethod
